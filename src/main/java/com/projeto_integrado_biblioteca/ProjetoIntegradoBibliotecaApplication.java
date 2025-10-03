31abe1efbe9f6d0b0038f4a57046f24cf917c47b
Ariel Rodrigues <ariel.dev22@gmail.com>
2025-10-03 10:34:09 -0300

    Initial commit

---
+++ src/main/java/com/projeto_integrado_biblioteca/ProjetoIntegradoBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto_integrado_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegradoBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegradoBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto_integrado_biblioteca/ProjetoIntegradoBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto_integrado_biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoIntegradoBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
