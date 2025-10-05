409b384856601277ece0c76e7355b7984b8fe939
Ariel Rodriges <ariel.dev22@gmail.com>
2025-10-05 14:07:26 -0300

    feat: implementação de UserDetailsService

---
+++ src/main/java/com/projeto_integrado_biblioteca/security/UserDetailsServiceImpl.java
@@ -0,0 +1,19 @@
+package com.projeto_integrado_biblioteca.security;
+
+import com.projeto_integrado_biblioteca.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
+    }
+}
