a482558e844737ef4604df803ab1d7a802fe2ef2
Ariel Rodriges <ariel.dev22@gmail.com>
2025-10-05 14:08:59 -0300

    feat: SecurityFilter

---
+++ src/main/java/com/projeto_integrado_biblioteca/security/SecurityFilter.java
@@ -0,0 +1,46 @@
+package com.projeto_integrado_biblioteca.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityFilter extends OncePerRequestFilter {
+    private final TokenService tokenService;
+    private final UserDetailsServiceImpl detailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = recoverToken(request);
+
+        if (token != null) {
+            String username = tokenService.validateToken(token);
+
+            if (username != null) {
+                var user = detailsService.loadUserByUsername(username);
+
+                var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                throw new RuntimeException("Token inválido");
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+
+        if (header == null) return null;
+        else return header.replace("Bearer ", "");
+    }
+}
