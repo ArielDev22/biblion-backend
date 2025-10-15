a476a315aa0d75ad866275dff018fb77a98ff7f7
Ariel Rodriges <ariel.dev22@gmail.com>
2025-10-15 12:09:43 -0300

    feat: Genre entity

---
+++ src/main/java/com/projeto_integrado_biblioteca/domains/genre/Genre.java
@@ -0,0 +1,30 @@
+package com.projeto_integrado_biblioteca.domains.genre;
+
+import com.projeto_integrado_biblioteca.domains.book.Book;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_genres")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "genre_id")
+    private Long genreId;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "genres", fetch = FetchType.LAZY)
+    private Set<Book> books = new HashSet<>();
+}
