603cd9716c40ffbb3ddee0f4895e092d0be1cf86
Ariel Rodriges <ariel.dev22@gmail.com>
2025-10-15 11:44:44 -0300

    feat: novos exceptions handlers

--- src/main/java/com/projeto_integrado_biblioteca/exceptions/controller/AdviceController.java
+++ src/main/java/com/projeto_integrado_biblioteca/exceptions/controller/AdviceController.java
@@ -1,7 +1,13 @@
 package com.projeto_integrado_biblioteca.exceptions.controller;
 
-import com.projeto_integrado_biblioteca.exceptions.EmailAlreadyUsedException;
+import com.projeto_integrado_biblioteca.exceptions.ConflictException;
+import com.projeto_integrado_biblioteca.exceptions.InvalidRequestException;
+import com.projeto_integrado_biblioteca.exceptions.ResourceNotFoundException;
+import com.projeto_integrado_biblioteca.utils.LogMessageBuilder;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -10,19 +16,27 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.Objects;
 
+@Slf4j
 @ControllerAdvice
 public class AdviceController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AdviceController.class);
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleAnyException(Exception e, HttpServletRequest request) {
-        String message = e.getMessage();
         HttpStatus status = HttpStatus.BAD_REQUEST;
-        String path = request.getRequestURI();
+        LOGGER.error("💥 [500] Erro inesperado em {}: {}", request.getRequestURI(), e.getMessage(), e);
 
-        return ResponseEntity.status(status).body(buildResponse(message, status, path));
+        ErrorResponse response = buildResponse(
+                e.getMessage(),
+                status,
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(status).body(response);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -32,38 +46,91 @@ public class AdviceController {
     ) {
         String message = Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage();
         HttpStatus status = HttpStatus.BAD_REQUEST;
-        String path = request.getRequestURI();
 
-        return ResponseEntity.status(status).body(buildResponse(message, status, path));
+        LOGGER.warn(LogMessageBuilder.build(status, message, request));
+
+        ErrorResponse response = buildResponse(
+                e.getMessage(),
+                status,
+                request.getRequestURI()
+        );
+
+        return ResponseEntity.status(status).body(response);
     }
 
-    @ExceptionHandler(EmailAlreadyUsedException.class)
-    public ResponseEntity<ErrorResponse> handleEmailAlreadyUsed(
-            EmailAlreadyUsedException e,
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<ErrorResponse> handleConflict(
+            ConflictException e,
             HttpServletRequest request
     ) {
-        String message = e.getMessage();
         HttpStatus status = HttpStatus.CONFLICT;
-        String path = request.getRequestURI();
+        LOGGER.warn(LogMessageBuilder.build(status, e, request));
 
-        return ResponseEntity.status(status).body(buildResponse(message, status, path));
+        ErrorResponse response = buildResponse(
+                e.getMessage(),
+                status,
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(status).body(response);
     }
 
     @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<ErrorResponse> handleBadCredentialsException(
+    public ResponseEntity<ErrorResponse> handleBadCredentials(
             BadCredentialsException e,
             HttpServletRequest request
     ) {
-        String message = e.getMessage();
         HttpStatus status = HttpStatus.BAD_REQUEST;
-        String path = request.getRequestURI();
+        LOGGER.warn(LogMessageBuilder.build(status, e, request));
 
-        return ResponseEntity.status(status).body(buildResponse(message, status, path));
+        ErrorResponse response = buildResponse(
+                e.getMessage(),
+                status,
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(status).body(response);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFound(
+            ResourceNotFoundException e,
+            HttpServletRequest request
+    ) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        LOGGER.warn(LogMessageBuilder.build(status, e, request));
+
+        ErrorResponse response = buildResponse(
+                e.getMessage(),
+                status,
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(status).body(response);
+    }
+
+    @ExceptionHandler(InvalidRequestException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidRequest(
+            ResourceNotFoundException e,
+            HttpServletRequest request
+    ) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        LOGGER.warn(LogMessageBuilder.build(status, e, request));
+
+        ErrorResponse response = buildResponse(
+                e.getMessage(),
+                status,
+                request.getRequestURI()
+        );
+        return ResponseEntity.status(status).body(response);
     }
 
     private ErrorResponse buildResponse(String message, HttpStatus status, String path) {
-        Instant timestamp = LocalDateTime.now().toInstant(ZoneOffset.of("-03:00"));
+        Instant timestamp = ZonedDateTime.now(ZoneId.of("America/Sao_Paulo")).toInstant();
 
-        return new ErrorResponse(message, status, timestamp, path);
+        return new ErrorResponse(
+                timestamp,
+                status.value(),
+                status.getReasonPhrase(),
+                message,
+                path
+        );
     }
 }
