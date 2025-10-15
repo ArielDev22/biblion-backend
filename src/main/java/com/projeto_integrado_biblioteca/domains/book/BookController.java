f93b19dc9a37f6f1d664ee8da3f04dc77f78df6c
Ariel Rodriges <ariel.dev22@gmail.com>
2025-10-15 12:09:24 -0300

    feat: book admin & public controllers

---
+++ src/main/java/com/projeto_integrado_biblioteca/domains/book/BookController.java
@@ -0,0 +1,30 @@
+package com.projeto_integrado_biblioteca.domains.book;
+
+import com.projeto_integrado_biblioteca.domains.book.dto.BookDetailsResponse;
+import com.projeto_integrado_biblioteca.domains.book.dto.BookPreviewResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+@RequiredArgsConstructor
+public class BookController {
+    private final BookService bookService;
+
+    @GetMapping(value = "/home", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<BookPreviewResponse>> listPreviewBooks() {
+        return ResponseEntity.ok(bookService.listBookPreviews());
+    }
+
+    @GetMapping(value = "/details", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<BookDetailsResponse> getDetails(@RequestParam Long id) {
+        return ResponseEntity.ok(bookService.getDetails(id));
+    }
+}
